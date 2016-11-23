8fb312816394a55a506e9d673355e3ea98b2f908
danielcouceiro <danielcouceiro@gmail.com>
2016-11-23 20:05:45 +0000

    Implemented KPI - Average sales price

---
+++ Dashboard360/Dashboard360/Scripts/Financial/getAverageSalesPrice.js
@@ -0,0 +1,32 @@
+﻿var getTotalSales = function () {
+    console.log("getAverageSalesPrice");
+
+    var defer = $.Deferred();
+
+    $.getScript("Scripts/moment.min.js");
+
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:49751/api/sales",
+        success: function (sales) {
+            sales = JSON.parse(sales);
+            console.log(sales);
+            var totalSales = 0;
+            // Total in sales this year
+            for (var i = 0; i < sales.length; i++) {
+                totalSales += (sales[i].TotalIva + sales[i].TotalMerc);
+            }
+            console.log("totalSales: " + totalSales);
+            $(".averageSalesPrice").append("<p> " + (totalSales / sales.length).toFixed(2) + "€ </p>");
+
+        }
+    }).fail(function () {
+        alert("ERROR: getting sales list");
+    });
+    setTimeout(function () {
+        defer.resolve();
+    }, 5000);
+    return defer;
+};
+
+getTotalSales();
