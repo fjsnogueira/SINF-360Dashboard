a620e70314f3ed6d59874d6d5f85decc6ab9a79b
danielcouceiro <danielcouceiro@gmail.com>
2016-11-23 19:28:07 +0000

    Quick fix

--- Dashboard360/Dashboard360/Controllers/FinancialController.cs
+++ Dashboard360/Dashboard360/Controllers/FinancialController.cs
@@ -5,8 +5,6 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using System.Threading.Tasks;
-
-using Dashboard360.Items;
 using Dashboard360.Lib_Primavera.Model;
 
 namespace Dashboard360.Controllers
