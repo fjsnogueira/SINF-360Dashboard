cf3995a37aed375cabb3b7d3c11560dbb0b88801
Joao Vieira de Castro <manueljoaocastro@gmail.com>
2016-12-14 14:18:45 +0000

    Box spacing done

--- Dashboard360/Dashboard360/Scripts/Clients/clientDetailsManager.js
+++ Dashboard360/Dashboard360/Scripts/Clients/clientDetailsManager.js
@@ -24,6 +24,8 @@ var getAllSales = function () {
                     var liquid = 0;
                     for (var j = 0; j < temp.LinhasDoc.length; j++) {
                         liquid += temp.LinhasDoc[j].TotalLiquido;
+                        if (temp.LinhasDoc[j].TotalLiquido > 0)
+                            $(".sales-modal-body").append("<tr> <td>" + temp.LinhasDoc[j].DescArtigo + "</td><td>" + moment.utc(temp.Data).format('LLL') + "</td><td style='text-align: right;'>" + formatPrice(temp.LinhasDoc[j].TotalLiquido.toFixed(2)) + "€ </td></tr>");
                     }
                     var month = moment(temp.Data).month();
                     if (month == 12 || month == 1 || month == 2)
@@ -35,39 +37,49 @@ var getAllSales = function () {
                     if (month == 9 || month == 10 || month == 11)
                         fall++;
                     // Modal Total Sales
-                    $(".sales-modal-body").append("<tr> <td>" + temp.TipoDoc + "</td><td>" + moment.utc(temp.Data).format('LLL') + "</td><td style='text-align: right;'>" + formatPrice(liquid.toFixed(2)) + "€ </td></tr>");
+
                     totalEver += liquid;
                     if (moment(temp.Data).year() == year) {
                         totalThisYear += liquid;
                         totalSalesThisYear++;
                         // Modal Year Sales
-                        $(".sales-year-modal-body").append("<tr> <td>" + temp.TipoDoc + "</td><td>" + moment.utc(temp.Data).format('LLL') + "</td><td style='text-align: right;'>" + formatPrice(liquid.toFixed(2)) + "€ </td></tr>");
+                        for (var k = 0; k < temp.LinhasDoc.length; k++) {
+                            if (temp.LinhasDoc[k].TotalLiquido > 0)
+                                $(".sales-year-modal-body").append("<tr> <td>" + temp.LinhasDoc[k].DescArtigo + "</td><td>" + moment.utc(temp.Data).format('LLL') + "</td><td style='text-align: right;'>" + formatPrice(temp.LinhasDoc[k].TotalLiquido.toFixed(2)) + "€ </td></tr>");
+                        }
+
                     }
-                    
+
                 }
                 if (temp.TipoDoc === "ECL") {
                     var ecl = 0;
                     for (var j = 0; j < temp.LinhasDoc.length; j++) {
                         ecl += temp.LinhasDoc[j].TotalLiquido;
+                        if (temp.LinhasDoc[j].TotalLiquido > 0)
+                            $(".orders-modal-body").append("<tr> <td>" + temp.LinhasDoc[j].DescArtigo + "</td> <td>" + moment.utc(temp.Data).format('LLL') + "</td><td style='text-align: right;'>" + formatPrice(temp.LinhasDoc[j].TotalLiquido.toFixed(2)) + "€ </td></tr>");
                     }
-                    $(".orders-modal-body").append("<tr> <td>" + moment.utc(temp.Data).format('LLL') + "</td><td style='text-align: right;'>" + formatPrice(ecl.toFixed(2)) + "€ </td></tr>");
                     totalOrders += ecl;
                 }
             }
+
             $(".totalSales").append(formatPrice(Math.floor(totalEver).toString()) +
                " <span style='font-size: 20px!important;'>" + totalEver.toString().split(".")[1].slice(0, 2) + "</span>");
             $(".loadingSalesTotal").hide();
+
             $(".totalSalesThisYear").append(formatPrice(Math.floor(totalThisYear).toString()) +
                " <span style='font-size: 20px!important;'>" + totalThisYear.toString().split(".")[1].slice(0, 2) + "</span>");
             $(".loadingSalesTotalThisYear").hide();
+
             var avg = totalEver / totalSales;
             $(".averageSale").append(formatPrice(Math.floor(avg).toString()) +
                " <span style='font-size: 20px!important;'>" + avg.toString().split(".")[1].slice(0, 2) + "</span>");
             $(".loadingAverageSale").hide();
+
             var avgYear = totalThisYear / totalSales
             $(".averageSaleThisYear").append(formatPrice(Math.floor(avgYear).toString()) +
                " <span style='font-size: 20px!important;'>" + avgYear.toString().split(".")[1].slice(0, 2) + "</span>");
             $(".loadingAverageSaleThisYear").hide();
+
             $(".totalOrders").append(formatPrice(Math.floor(totalOrders).toString()) +
                " <span style='font-size: 20px!important;'>" + totalOrders.toString().split(".")[1].slice(0, 2) + "</span>");
             $(".loadingTotalOrders").hide();
@@ -127,7 +139,7 @@ var getTopProducts = function () {
         url: "http://localhost:49751/api/Products/topProductsByClient/" + clientID,
         success: function (products) {
             products = JSON.parse(products);
-         //   console.log(products);
+            //   console.log(products);
             var productData = [];
             var descriptions = [];
             for (var i = 0; i < products.length; i++) {
